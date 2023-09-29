b8c390defd6d44825774c2747243723326fa9d99
kmc369 <keishacoleman710@gmail.com>
2023-09-29 16:35:11 -0400

    react readded

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request 
 from flask_login import login_required
-from app.models import Product,User
-
+from app.models import Product,User,db
+from ..forms.product_form import ProductForm
 from .aws_helpers import get_unique_filename,upload_file_to_s3,remove_file_from_s3
 
 
@@ -25,9 +25,69 @@ def get_product_by_id(userid):
     products = Product.query.filter_by(user_id=userid)
     return [product.to_dict() for product in products]
 
-@products_bp.route('/', methods=["POST"])
+@products_bp.route('/new_product', methods=["POST"])
 def create_product():
+ 
     """create a new product """
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+     
+        # image = form.data["image"]
+        # image.filename = get_unique_filename(image.filename)
+        # upload = upload_file_to_s3(image)
+        # if "url" not in upload:
+        #     return jsonify({"error": "Failed to upload image to S3"}), 400
+        # image_url = upload["url"]
+        
+        # image1 = form.data["image1"]
+        # image1.filename = get_unique_filename(image1.filename)
+        # upload1 = upload_file_to_s3(image1)
+        # if "url" not in upload1:
+        #     return jsonify({"error": "Failed to upload image1 to S3"}), 400
+        # image1_url = upload1["url"]
+
+        # image2 = form.data["image2"]
+        # image2.filename = get_unique_filename(image2.filename)
+        # upload2 = upload_file_to_s3(image2)
+        # if "url" not in upload2:
+        #     return jsonify({"error": "Failed to upload image2 to S3"}), 400
+        # image2_url = upload2["url"]
+
+        # image3 = form.data["image3"]
+        # image3.filename = get_unique_filename(image3.filename)
+        # upload3 = upload_file_to_s3(image3)
+        # if "url" not in upload3:
+        #     return jsonify({"error": "Failed to upload image3 to S3"}), 400
+        # image3_url = upload3["url"]
+        
+        new_product = Product(
+            price=form.data["price"],
+            image=form.data["image"],
+            image1=form.data["image1"],
+            image2=form.data["image2"],
+            image3=form.data["image3"],
+            title=form.data["title"],
+            handmade=form.data["handmade"],
+            vintage=form.data["vintage"],
+            made_to_order=form.data["made_to_order"],
+            creator=form.data["creator"],
+            material=form.data["material"],
+            description=form.data["description"],
+            user_id=form.data["user_id"] 
+            
+        )
+        db.session.add(new_product)
+        db.session.commit()
+        return jsonify(new_product.to_dict(), 201)
+    else:
+        return jsonify({"error": form.errors}), 400
+    
+    
+        
+        
+    
+    
     
     
     
--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -2,26 +2,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,IntegerField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
-from wtforms import StringField,IntegerField
-from flask_wtf.file import FileField, FileRequired,FileAllowed,BooleanField,FieldList
+from wtforms import StringField,IntegerField,BooleanField,FieldList
+from flask_wtf.file import FileField, FileRequired,FileAllowed
 from wtforms.validators import DataRequired
 
  
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
-
-class Product(FlaskForm):
+# FileRequired()
+class ProductForm(FlaskForm):
     price = IntegerField("price", validators=[DataRequired()])
-    image = FileField("Image file", validators=[FileRequired,FileAllowed(list(ALLOWED_EXTENSIONS))])
+    image = FileField("Image file", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     image1 = FileField("Image file", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     image2 = FileField("Image file", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     image3 = FileField("Image file", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     title = StringField("title",validators=[DataRequired()])
-    handmade = BooleanField("handmade",validators=[DataRequired()])
-    vintage =BooleanField("vintage",validators=[DataRequired()])
-    made_to_order = BooleanField("made_to_order",validators=[DataRequired()])
+    handmade = BooleanField("handmade")
+    vintage =BooleanField("vintage")
+    made_to_order = BooleanField("made_to_order")
     creator  = StringField("creator",validators=[DataRequired()])
     material = StringField("material",validators=[DataRequired()])
     description = StringField("description",validators=[DataRequired()])
-    shopping_cart_id = IntegerField("shopping_cart")
     user_id = IntegerField('User ID', validators=[DataRequired()])
-    # reviews = FieldList(StringField("reviews"), min_entries=0, validators=[DataRequired()])
