4f85df445431911142e0e56616f8213d5a23258c
kmc369 <keishacoleman710@gmail.com>
2023-09-29 06:17:56 -0400

    main first push

--- app/models/product.py
+++ app/models/product.py
@@ -23,7 +23,7 @@ class Product(db.Model):
     shopping_cart_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("shopping_carts.id"),nullable=False))
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
 
-    #relationsips s
+    #relationsips 
     user = db.relationship("User", back_populates="products")
     reviews = db.relationship("Review", back_populates="products", cascade="all, delete-orphan")
     
