9bae0ea862ea0e98249cb7340431026cb236e1e7
kmc369 <keishacoleman710@gmail.com>
2023-09-30 09:17:50 -0400

    product thunks started

--- react-app/src/store/products.js
+++ react-app/src/store/products.js
@@ -1,7 +1,7 @@
 
 //ACTION TYPES
 const GET_PRODUCTS = 'get/products'
-
+const DELETE_PRODUCT='delete/product'
 
 
 
@@ -13,6 +13,14 @@ export const get_products = (data) =>{
     }
 }
 
+export const delete_product = (data)=>{
+    return {
+        type:DELETE_PRODUCT,
+        payload:data
+    }
+}
+
+
 
 
 
@@ -21,14 +29,20 @@ export const get_products = (data) =>{
 
 
 // THUNKS
+export const deleteProductThunk = (product_id) => async(dispatch,getState) =>{
+    const res = await fetch(`api/products/delete/${product_id}`,{
+        method:"DELETE"
+    })
+    if(res.ok){
+        const data = await res.json()
+        dispatch(delete_product(data))
+        return data
+    }
+}
 export const getAllProductThunk = () => async (dispatch, getState) => {
-
-   
-        const res = await fetch('/api/products/',{
+         const res = await fetch('/api/products/',{
             method: "GET"
         });
-
-   
         if (res.ok) {
             const data = await res.json();
             dispatch(get_products(data));
@@ -37,8 +51,7 @@ export const getAllProductThunk = () => async (dispatch, getState) => {
           
             const errorData = await res.json();
             throw new Error(errorData.error || 'Failed to fetch data');
-        }
-    
+        }  
 }
 
 
@@ -48,12 +61,18 @@ const inital_state = {allProducts:{}}
 const productReducer = (state=inital_state, action)=>{
    
     switch(action.type) {
-        case GET_PRODUCTS:
+        case GET_PRODUCTS:{
         
             const newState = {...state, allProducts:{...state.allProducts}}
             //make a copy of all the keys in the data not whats in them
             newState.allProducts = action.payload
             return newState
+        }
+        case DELETE_PRODUCT:{
+            const newState = {...state,allProducts:{...state.allProducts}}
+            newState.allProducts = action.payload
+            return newState
+        }
         default:
             return state
 
