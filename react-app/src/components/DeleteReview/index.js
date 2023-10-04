c2abe78e071a706ffe538436181be66030af1f90
kmc369 <keishacoleman710@gmail.com>
2023-10-03 21:16:14 -0400

    reviews Done

---
+++ react-app/src/components/DeleteReview/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch,useSelector  } from "react-redux";
+import * as ProductActions from '../../store/products'
+
+function DeleteReview({prop,index}){
+    console.log('the prop is',prop, index)
+    const dispatch = useDispatch()
+
+const handleDelete = async(e)=>{
+    e.preventDefault()
+        await  dispatch(ProductActions.deleteReviewThunk(prop.id,index))
+  }
+
+  return (
+
+    <>
+    <h1>Are you sure you want to delete</h1>
+    <button onClick={handleDelete}>Yes</button>
+    
+    </>
+  )
+
+
+
+}
+export default DeleteReview
