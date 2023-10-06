e8bc3e7b2390b228eff6295839a52de9a518034d
kmc369 <keishacoleman710@gmail.com>
2023-10-06 09:45:28 -0400

    CSS

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -48,25 +48,52 @@ const fetchData = async () => {
         <div className="header">
           <div className="name-image">
           <img src={sessionUser.image} style={{ height: "40px", width: "40px", borderRadius: "25px" }} />
-          {sessionUser.username}
+            <u onClick={()=>history.push(`userProfile/${sessionUser.id}`)}>{sessionUser.username}</u>
           </div>
-          <button className="create-product" onClick={() => history.push("/new_product")}>Create Product</button>
+
+          <div className="shop-name-product">
+          <button className="create-product-button" onClick={() => history.push("/new_product")}>New Product</button>
+          <p className="user-shop">{`${sessionUser.username}'s Shop`}</p>
+          </div>
         </div>
       </div>
 
       <div className="products-containers">
         <div className="product-items">
           {values.map((element, index) => (
-            <div key={index} className={`product${index}`}>
-              <img src={element.image} style={{ height: "200px", width: "200px" }} />
-              <span><OpenModalButton modalComponent={<DeleteProductModal prop={element} buttonText="Delete Product" onDelete={() => handleDelete(element.id)}/>}/></span>
-              <span><button  onClick={() => history.push(`/edit_product/${element.id}`)}>Edit Product</button></span>
+            <div key={index} id="product-user" className={`productz${index}` }>
+              <img className="user-image-product" src={element.image} style={{ height: "230px", width: "230px",borderRadius:"10px" }} />
+              <div className="manage-buttons">
+              <span><OpenModalButton className="delete-product" buttonText="Delete Product" modalComponent={<DeleteProductModal prop={element}  onDelete={() => handleDelete(element.id)}/>}/></span>
+              <span><button className="edit-product"  onClick={() => history.push(`/edit_product/${element.id}`)}>Edit Product</button></span>
+              </div>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="extra-stuff"></div>
+      <div className="extra-stuff">
+          <div className="footer-etsy">
+            <i class="fa-solid fa-globe" style={{color:"white"}}> </i>
+            <div className="powered">Etsy is powered by 100% renewable electricity.</div>
+            </div>
+
+          <div className="footer-container">
+            <div className="fetsy-download">
+                <div className="Fetsy-app" onClick={()=>{history.push('/')}}>Fetsy</div>
+                <div className="download-word" onClick={()=>{history.push('/')}} >Download the Fetsy App</div>
+            </div>
+
+            <div className="shop-contact">
+              <div className="contact-us">Contact us </div>
+              <div className="socials">
+                <div className="linkedIn"> linkedIn</div>
+                <div className="gitHub"> GitHub</div>
+                <div className="Twitter"> Twitter</div>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
 
 
