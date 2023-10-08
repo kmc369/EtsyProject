e871e7485616a960d1c49f75cca1cdd49cab534e
kmc369 <keishacoleman710@gmail.com>
2023-10-08 17:31:37 -0400

    footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -22,9 +22,9 @@ function Footer(){
             <div className="shop-contact">
               <div className="contact-us">Contact us </div>
               <div className="socials">
-                <a href="https://www.linkedin.com/in/keisha-coleman-1657bb1b4/"className="linkedIn"><i class="fa-brands fa-linkedin"style={{fontSize:"30px"}}></i> linkedIn</a>
-                <a href="https://github.com/kmc369" className="gitHub" ><i class="fa-brands fa-github"  style={{fontSize:"30px"}}></i> GitHub</a>
-                <a href="https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" className="Twitter"> <i class="fa-brands fa-twitter" style={{fontSize:"30px"}}></i>Twitter</a>
+                <a href="https://www.linkedin.com/in/keisha-coleman-1657bb1b4/"className="linkedIn"><i class="fa-brands fa-linkedin"style={{fontSize:"20px"}}></i> linkedIn</a>
+                <a href="https://github.com/kmc369" className="gitHub" ><i class="fa-brands fa-github"  style={{fontSize:"20px"}}></i> GitHub</a>
+                <a href="https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" className="Twitter"> <i class="fa-brands fa-twitter" style={{fontSize:"20px"}}></i>Twitter</a>
               </div>
             </div>
           </div>
