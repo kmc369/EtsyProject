337011446cdeacd6733e15888403a6e6e897d68c
kmc369 <keishacoleman710@gmail.com>
2023-10-09 09:43:13 -0400

    css updates

--- react-app/src/components/DeleteProductModal/index.js
+++ react-app/src/components/DeleteProductModal/index.js
@@ -4,28 +4,32 @@ import { useDispatch,useSelector  } from "react-redux";
 import * as UserAction from '../../store/session'
 import UserProfile from "../UserProfile";
 import { useHistory } from "react-router-dom/";
-
+import { useModal } from '../../context/Modal';
 function DeleteProductModal ({ prop, buttonText, onDelete }){
     const dispatch = useDispatch()
     const history = useHistory()
      const sessionUser = useSelector(state=>state.session.user)
+     const { closeModal } = useModal();
 
     const handleDelete = async () => {
       dispatch(ProductActions.deleteProductThunk(prop.id))
       await onDelete(prop.id);
       history.push(`/userProfile/${sessionUser.id}`)
+      closeModal()
     };
 
       return (
           <>
-          <div>
-         <div className="delete-spot">
-        <h2>Confirm Delete</h2>
-        <p>Are you sure you want to remove this spot from the listing?</p>
-        <div><button className='confirm'  type='submit' onClick={handleDelete} >Yes, Delete Spot</button></div>
-        <div><button className='deny' >No, Keep Spot</button></div>
+          <>
+    <div className="deleteModel-container">
+      <h2 className="delete-review-word">Are you sure you want to delete this Product</h2>
+      <div> <p className="delete-review-word1">This action can not be undone</p></div>
+      <div className="buttonItems">
+        <button className="delete-button1"onClick={handleDelete}>Yes</button>
+        <button className="delete-button" onClick={closeModal}>NO</button>
       </div>
-      </div>
+    </div>
+    </>
           </>
       )
       
