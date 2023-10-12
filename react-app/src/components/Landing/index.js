fadcb0e8369ecc2a1e8302826a70568e804f7a2c
kmc369 <keishacoleman710@gmail.com>
2023-10-12 18:47:26 -0400

    landing fixed

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -14,15 +14,38 @@ const history = useHistory()
 
 
 
+const breakFour = (products)=>{
+  console.log("values in breaking four", products)
+  const groups = [];
+
+  for(let i=0 ; i<products.length;i+=4){
+
+   const segment= products.slice(i, i+4)
+   console.log("eaach segment is",segment)
+   groups.push(segment)
+  }
+  
+  return groups
+
+}
+
 
 
 
 
 useEffect(()=>{
+
+
+
+
+
+  
     const products = async() =>{
         await dispatch(ProductActions.getAllProductThunk())
     }
     products()
+    
+    
 },[dispatch])
 
 if(Object.values(products).length===0){
@@ -30,118 +53,64 @@ if(Object.values(products).length===0){
 }
 const values = Object.values(products)
 
+
 return (
-    <>  
+  <>
     <div className="welcom3-container">
       <div className="session-container">
-    {sessionUser? (
-        <h3 className="landing-header">Welcome back, <u>{sessionUser.username}</u> </h3>
-
-    ):(
-        <h3 className="landing-header">Hello Welcome to Fetsy</h3>
-    )}
-    </div>
+        {sessionUser ? (
+          <h3 className="landing-header">Welcome back, <u>{sessionUser.username}</u> </h3>
+        ) : (
+          <h3 className="landing-header">Hello Welcome to Fetsy</h3>
+        )}
+      </div>
     </div>
     <div className="magical-container">
-    
-          <div className="underth">Gifts Under $30 </div>
-          <div className="magic">Magical, meaningful gifts!</div>
-          <p className="part"> Participating shops only. Terms apply.</p>
-      
+      <div className="underth">Gifts Under $30 </div>
+      <div className="magic">Magical, meaningful gifts!</div>
+      <p className="part"> Participating shops only. Terms apply.</p>
     </div>
 
     <div className="circle-images">
-      <img src="https://i.etsystatic.com/30981785/r/il/393034/4645837193/il_794xN.4645837193_qkwb.jpg" style={{width:"130px",height:"130px"}}/>
-      <img src="https://i.etsystatic.com/17305851/r/il/319e0d/4796740674/il_794xN.4796740674_mypt.jpg" style={{width:"130px",height:"130px"}}/>
-      <img src="https://i.etsystatic.com/11753776/r/il/51dcbb/1975020708/il_794xN.1975020708_1t4i.jpg" style={{width:"130px",height:"130px"}}/>
-      <img src="https://i.etsystatic.com/32516685/r/il/2f2f23/4963062963/il_794xN.4963062963_aisj.jpg" style={{width:"130px",height:"130px"}}/>
-      <img src="https://i.etsystatic.com/25199519/r/il/3df845/4968305885/il_794xN.4968305885_l8pb.jpg" style={{width:"130px",height:"130px"}}/>
-
-    </div >
-
-      <div className="entire-landing-container">
-      <div className="landing-center-container">
-      <div className="because-image-container">
-    <div className="because-container">
-    <p>Because You viewed</p>
-      <div><img src={values[0].image} style={{ width: '50px', height: '50px', borderRadius:"5px" }} /></div>
-      </div>
-     <div className="landing-container" >
-      
-    
-        {values.slice(0,4).map((element, index) => (
+      {/* Your image elements go here */}
+    </div>
          
-          <div key={index} className="product" id="products" onClick={()=>{history.push(`/products/${element.id}`)}}>
-              
-           
-            <img src={element.image} alt={`Product ${index}`} style={{ width: '200px', height: '200px' }} />
-            <div className="price">
-            <i class="fa-solid fa-dollar-sign"  style={{color: "#000000",marginRight:"5px"}}></i>
-            {element.price}
-          
-            </div>
-  
-           </div>
-    
-        ))}
-      </div>
-      </div>
-      <div className="because-image-container">
-      <div className="because-container">
+    <div className="center-container">
+    <div className="landing-container">
+  {breakFour(values).map((element, index) => (
+    <div className="holding-item" key={index}> 
+       <div className="because-container">
       <p>Because You viewed</p>
-      <div><img src={values[4].image} style={{ width: '50px', height: '50px' }} /></div>
-      </div>
-      <div className="landing-container" >
-        {values.slice(4,8).map((element, index) => (
-          <div key={index} className="product" id="products" onClick={()=>{history.push(`/products/${element.id}`)}}>
-         
-           
-            <img src={element.image} alt={`Product ${index}`} style={{ width: '200px', height: '200px' }} />
-            <div className="price">
-            <i class="fa-solid fa-dollar-sign"  style={{color: "#000000",marginRight:"5px"}}></i>
-            {element.price}
-          
-            </div>
-  
-           </div>
-    
-        ))}
-      </div>
+      <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf4UFLFtv4aUBTA925XFEFIpZdOwxlFmx4K2dJWKiIP4gy4S-je3UCsmt6INgfxZATOrE&usqp=CAU" style={{ width: '50px', height: '50px' }} /></div>
       </div>
 
-
- <div className="because-image-container">
-    <div className="because-container">
-      <p>Because You viewed</p>
-      <div><img src={values[8].image} style={{ width: '50px', height: '50px' }} /></div>
-      </div>
-      <div className="landing-container" >
-        
-        {values.slice(8,12).map((element, index) => (
-          <div key={index} className="product" id="products" onClick={()=>{history.push(`/products/${element.id}`)}}>
-         
-           
-            <img src={element.image} alt={`Product ${index}`} style={{ width: '200px', height: '200px' }} />
-            <div className="price">
-            <i class="fa-solid fa-dollar-sign"  style={{color: "#000000",marginRight:"5px"}}></i>
-            {element.price}
-          
-            </div>
-  
-           </div>
-    
-        ))}
-      </div>
-      </div>
-      </div>
-      </div>
+      <div className="images-and-price">
+      {element.map((segelement, index) => (
+        <div key={index} className="product" id="products" onClick={() => { history.push(`/products/${segelement.id}`) }}>
+          <img src={segelement.image} style={{ width: '200px', height: '200px' }} alt={`Product ${index + 1}`} />
+          <div className="price">
+            <i className="fa-solid fa-dollar-sign" style={{ color: "#000000", marginRight: "5px" }}></i>
+            {segelement.price}
+          </div>
+        </div>
+       
+      ))}
+       </div>
+    </div>
+  ))}
+</div>
+</div>
 
 
-    
-  
-          <Footer/>
-    </>
-  );
+
+
+
+
+
+
+    <Footer />
+  </>
+);
 }
 
 
