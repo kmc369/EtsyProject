d953707e435ae699857fda73fd05b4d666897b50
kmc369 <keishacoleman710@gmail.com>
2023-10-26 18:41:27 -0400

    search better

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -11,32 +11,59 @@ function Search(){
     const {search} = useParams()
     const history = useHistory()
     const dispatch = useDispatch()
-	const [searchData,setSearchData] = useState([])
+	  const [searchData,setSearchData] = useState([])
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+
+    const images = [
+      'https://www.fotor.com/blog/wp-content/uploads/2019/08/Untitled-design-2019-08-15T165127.478.jpg',
+
+
+      'https://www.fotor.com/blog/wp-content/uploads/2019/08/Untitled-design-2019-08-15T152106.649.jpg',
+
+
+      "https://www.fotor.com/blog/wp-content/uploads/2019/08/Etsy-Shop-Banner.jpg",
+      
+      // "https://www.constantcontact.com/blog/wp-content/uploads/2019/01/Artboard-2.png",
+
+      "https://www.fotor.com/blog/wp-content/uploads/2019/08/Untitled-design-2019-08-15T164109.840.jpg",
+      
+      'https://www.fotor.com/blog/wp-content/uploads/2019/08/Untitled-design-2019-08-15T164617.072.jpg',
+      "https://www.fotor.com/blog/wp-content/uploads/2019/08/Untitled-design-2019-08-15T164713.036.jpg",
+    ];
 
     useEffect(()=>{
 
+      
+
         const fetchData = async() =>{
-       
-       
 			    const data = await dispatch(SearchAction.getSearchThunk(search))
-        
-          // if(data.length===0){
-           
-            // history.push("/")
-          // }
 			     setSearchData(data)
-       
-       
-     
     }
+    
     fetchData()
     },[dispatch,search])
 
-   
+    const updateImageIndex = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+  
+    useEffect(() => {
+      // Create a timer to change images every 2 seconds
+      const timer = setInterval(updateImageIndex, 3000);
+  
+      // Clean up the timer when the component unmounts
+      return () => {
+        clearInterval(timer);
+      };
+    }, [images]);
   
     return (
         <>
         <div className="search-entire-frame">
+          <div className="top-image-search">
+            <img className="image-search-interval" src={images[currentIndex]} alt="Slider" />
+        </div>
           <div className="search-container-frame" >
         {searchData.length===0 ? (
           
